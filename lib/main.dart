ade81bd5a8d248b391ce5d79f34f3a7a9be638e8
ayush-921 <gupta.ayush16289@gmail.com>
2022-06-21 14:58:33 +0530

    initialize app changes for web

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_insta_clone/responsive/mobile_screen_layout.dart';
 import 'package:flutter_insta_clone/responsive/responsive_screen_layout.dart';
 import 'package:flutter_insta_clone/responsive/web_screen_layout.dart';
+import 'package:flutter_insta_clone/screens/login_screen.dart';
 import './utils/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyAZEFF9_LrrqwnV0Bj5nxgdqUhMmc2zCg8",
+          appId: "1:165784649696:web:67f0847e21e58b37017585",
+          messagingSenderId: "165784649696",
+          projectId: "flutter-insta-clone-5c983"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -24,9 +34,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: const ResponsiveLayout(
-          webScreenLayout: WebScreenLayout(),
-          mobileScreenLayout: MobileScreenLayout()),
+      // home: const ResponsiveLayout(
+      //     webScreenLayout: WebScreenLayout(),
+      //     mobileScreenLayout: MobileScreenLayout()),
+      home: LoginScreen(),
     );
   }
 }
