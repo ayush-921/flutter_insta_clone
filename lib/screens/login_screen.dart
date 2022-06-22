ef850a675684f6d0134d74daa38d28ac01485b6e
ayush-921 <gupta.ayush16289@gmail.com>
2022-06-22 12:07:20 +0530

    added text field in login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '/utils/colors.dart';
+import 'package:flutter_insta_clone/widgets/input_textField.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,10 +11,56 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Login Screen'),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+              //instagram svg logo
+              SvgPicture.asset(
+                'assets/ic_instagram.svg',
+                color: primaryColor,
+                height: 64,
+              ),
+
+              const SizedBox(
+                height: 64,
+              ),
+
+              //email text field
+              InputTextField(
+                textEditingController: _emailController,
+                hintText: "Email",
+                textInputType: TextInputType.emailAddress,
+              ),
+
+              const SizedBox(
+                height: 24,
+              ),
+
+              //pasword text field
+              InputTextField(
+                textEditingController: _passController,
+                hintText: "Password",
+                textInputType: TextInputType.text,
+                isPass: true,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
