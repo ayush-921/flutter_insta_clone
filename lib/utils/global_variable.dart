b7b9b36f8e73f6b8359c869df4c81ef81e846b3d
ayush-921 <gupta.ayush16289@gmail.com>
2022-07-13 17:20:26 +0530

    added FeedScreen Widget in homeScreenItem

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_insta_clone/screens/feeds_screen.dart';
 
 import '../screens/add_post_screen.dart';
 
 const webScreenSize = 600;
 
 List<Widget> homeScreenItem = [
-  Text('feed'),
+  FeedScreen(),
   Text('seach'),
   AddPostScreen(),
   Text('Fav'),
